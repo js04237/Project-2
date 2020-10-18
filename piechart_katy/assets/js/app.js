714e3626a2bbeffc31a898d9d58997eaf98d7a52
CatherineLuquire <cluquire@gmail.com>
2020-10-18 14:57:21 -0500

    pie chart complete

--- piechart_katy/assets/js/app.js
+++ piechart_katy/assets/js/app.js
@@ -1,105 +1,72 @@
 function buildPlot(podChoice) {
 
-url = "http://hotline.whalemuseum.org/api.json?limit=10000";
-d3.json(url).then(function(whaledata) {
-    // console.log(whaledata);
-    j = 0
-    k = 0
-    l = 0
-    jk = 0
-    jl = 0
-    kl = 0
-    jkl = 0
-    for (var i = 0; i < whaledata.length; i++) {
-        var pod = whaledata[i].orca_pod;
-        // console.log(pod)
-        if (pod == "j") {
-            j = j +1;
-        }
-        if (pod == "k") {
-            k = k +1;
-        }
-        if (pod == "l") {
-            l = l +1;
-        }
-        if (pod == "j, k") {
-            jk = jk +1;
-        }
-        if (pod == "j, l") {
-            jl = jl +1;
-        }
-        if (pod == "k, l") {
-            kl = kl +1;
-        }
-        if (pod == "j, k, l") {
-            jkl = jkl +1;
-        }
-    }
-      var j_values = [j, jk, jl, jkl];
-      var k_values = [k, jk, kl, jkl];
-      var l_values = [l, jl, kl, jkl];
-      var j_labels = ['J Pod', "J+K Pods", "J+L Pods", "All Pods"];
-      var k_labels = ['K Pod', 'J+K Pods', "K+L Pods", "All Pods"];
-      var l_labels = ['L Pod', 'J+L Pods', "K+L Pods", "All Pods"];
+    url = "http://hotline.whalemuseum.org/api.json?limit=10000";
+    d3.json(url).then(function (whaledata) {
+        //console.log(whaledata);
+        var tdata = whaledata.filter(d => d.hasOwnProperty("orca_pod"));
+        console.log(tdata)
+        var pod_counts = tdata.reduce(function(obj, v) {
+            obj[v.orca_pod] = (obj[v.orca_pod] || 0) + 1;
+            return obj;
+        }, {})
+        console.log(pod_counts);
+        console.log(podChoice);
+        var final_data = {};
+        var filter_value = podChoice === "J Pod" ?
+            "j" : podChoice === "K Pod" ? "k" : 
+            "l"
+        Object.keys(pod_counts).forEach((k) => {
+            if(k.includes(filter_value)) {
+                final_data[k] = pod_counts[k];
+            }
+        });
+
+        console.log(filter_value)
+        console.log(final_data)
 
 
-
-// function buildPlot(podChoice) {
-    var J_Pod = [j_values, j_labels];
-    var K_Pod = [k_values, k_labels];
-    var L_Pod = [l_values, l_labels];   
-    console.log(podChoice); 
-    if (podChoice = "J Pod") {
-        data = J_Pod;
-    }
-    if (podChoice = "K Pod") {
-        data = K_Pod
-    }
-    else  {
-        data = L_Pod
-    }
-      console.log(data);
-      
-    var data1 = [{
-            values: data[0],
-            labels: data[1],
+        var data_labels = [];
+        var data_values = [];
+        Object.keys(final_data).forEach((k) => {
+            data_labels.push(k);
+            data_values.push(final_data[k])
+        })
+        var data1 = [{
+            values: data_values,
+            labels: data_labels,
             type: 'pie'
-            }]
-    var layout = {
+        }]
+        var layout = {
             height: 400,
-            width: 500}
-        
-    Plotly.newPlot('piechart', data1, layout);
-        
+            width: 500
+        }
+        console.log(data1);
+        console.log(layout);
+        Plotly.newPlot('piechart', data1, layout);
+
     })
 };
 
-
-
-function optionChanged(podChoice){
+function optionChanged(podChoice) {
     console.log(podChoice);
     buildPlot(podChoice);
 }
-function update(data){
+function update(data) {
     buildPlot(data);
 };
 
-var pod_names = ["J Pod", "K Pod", "L Pod"];
-var dropdown = d3.select("#selDataset");
 
-pod_names.forEach(function(name) {
-    dropdown.append("option").text(name).property("value", name);    
-    // console.log(name);
-});
-var firstPod = pod_names[0];
-console.log(firstPod);
-buildPlot(firstPod);
-// function init() {
-//     update(jdata)
-// }
-// //     for (i = 0; pod_names.length; i++) {
-// //         dropdown.append("option").text(pod_names[i]);
+function init() {
+    var pod_names = ["J Pod", "K Pod", "L Pod"];
+    var dropdown = d3.select("#selDataset");
+    pod_names.forEach(function (name) {
+        dropdown.append("option").text(name).property("value", name);
+        // console.log(name);
+    });
+    var firstPod = pod_names[0];
+    console.log(firstPod);
+    buildPlot(firstPod);
 
-// // }};
+}
 
-// init();
+init();
